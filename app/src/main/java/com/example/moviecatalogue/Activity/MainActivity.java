43b345a7b18f6a52e2b8cb806bf7b09df7b6646c
Chandra Septian <cseptian48@gmail.com>
2019-09-05 16:07:16 +0700

    Local Storage

--- app/src/main/java/com/example/moviecatalogue/Activity/MainActivity.java
+++ app/src/main/java/com/example/moviecatalogue/Activity/MainActivity.java
@@ -2,81 +2,92 @@ package com.example.moviecatalogue.Activity;
 
 import android.content.Intent;
 import android.provider.Settings;
-import android.support.design.widget.TabLayout;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-import com.example.moviecatalogue.Fragment.MovieFragment;
+import com.example.moviecatalogue.Fragment.HomeFragment;
 import com.example.moviecatalogue.Fragment.ShowFragment;
 import com.example.moviecatalogue.R;
 
-public class MainActivity extends AppCompatActivity {
-
-    private SectionPagerAdapter sectionPagerAdapter;
-    private ViewPager viewPager;
+public class MainActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sectionPagerAdapter = new SectionPagerAdapter(getSupportFragmentManager());
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        viewPager = findViewById(R.id.container);
-        viewPager.setAdapter(sectionPagerAdapter);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-        TabLayout tabLayout = findViewById(R.id.tabLayout);
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
-
-
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.content, new HomeFragment()).commit();
+        }
     }
 
-    public class SectionPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionPagerAdapter(FragmentManager fm){
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            switch (position){
-                case 0:
-                    return new MovieFragment();
-                case 1:
-                    return new ShowFragment();
-            }
-            return null;
-        }
-
-        @Override
-        public int getCount() {
-            return 2;
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.setting_menu, menu);
-        return super.onCreateOptionsMenu(menu);
+        return false;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.setting) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        Fragment fragment = null;
+
+        if (id == R.id.nav_home) {
+            fragment = new HomeFragment();
+
+        } else if (id == R.id.nav_favorite) {
+            fragment = new ShowFragment();
+            getSupportActionBar().setTitle(getResources().getString(R.string.favorite));
+
+        } else if (id == R.id.nav_setting) {
             Intent i = new Intent(Settings.ACTION_LOCALE_SETTINGS);
             startActivity(i);
         }
-        return super.onOptionsItemSelected(item);
+
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.content, fragment).commit();
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
+
 }
 
